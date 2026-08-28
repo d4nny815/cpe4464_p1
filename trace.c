5fd0667ecdaa42849057bb99fc931580549d3720
Danny Gutierrez <dannygutierrez@Dannys-MacBook-Air-2.local>
2026-08-27 23:53:57 -0700

    printing Mac addresses

--- trace.c
+++ trace.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <pcap/pcap.h>
+#include <string.h>
+
 #include "trace.h"
+
+#define MAC_STR_LEN 19   // "xx:xx:xx:xx:xx:xx" + null
+
+
+void print_eth_header(uint8_t* data_ptr);
+
+
+void construct_mac_addr(char* mac_addr_str_buf, uint16_t p1, uint16_t p2, uint16_t p3);
+
 
 int main(int argc, char* argv[]) {
 
@@ -31,24 +42,57 @@ int main(int argc, char* argv[]) {
     // read packet by packet
     while (data_ptr) {
         // print metadata
-        printf("Packet number: %lu Packet Len: %d\n", ++pkt_cntr, pkt_header.caplen);
+        printf("\nPacket number: %lu Packet Len: %d\n", ++pkt_cntr, pkt_header.caplen);
 
-        ethernetHeader_t eth_header;
-        memcpy
-
-        // print ethernet header
-        printf("\t\tEthernet Header\n");
-        printf("\t\t\tDest MAC: %x\n", );
-        printf("lenk
+        print_eth_header((uint8_t*)data_ptr);
 
         data_ptr = pcap_next(pcap_ptr, &pkt_header);
     }
     
-    printf("%lu\n", sizeof(ethernetHeader_t));
-
     // close file
     pcap_close(pcap_ptr);
 
     return 0;
 }
+
+void print_eth_header(uint8_t* data_ptr) {
+    ethernetHeader_t eth_header;
+    memcpy(&eth_header, data_ptr, sizeof(eth_header));
+
+    char mac_addr[MAC_STR_LEN];
+
+    printf("\n\tEthernet Header\n");
+    construct_mac_addr(mac_addr, eth_header.src_addr1, eth_header.src_addr2, eth_header.src_addr3);
+    printf("\t\tDest MAC: %s\n", mac_addr);
+    printf("\t\tSource MAC: %s\n", mac_addr);
+    
+    switch (eth_header.type) {
+        case 0x0800:
+            printf("\t\tType: IPv4\n");
+            break;
+        case 0x0806:
+            printf("\t\tType: ARP\n");
+            break;
+        case 0x86DD:
+            printf("\t\tType: IPv6\n");
+            break;
+        default:
+            printf("\t\tType: Unknown\n");
+    }
+}
+
+void construct_mac_addr(char* mac_addr_str_buf, uint16_t p1, uint16_t p2, uint16_t p3) {
+    uint8_t p1_upper = p1 & 0xFF;
+    uint8_t p1_lower = p1 >> 8 & 0xFF;
+    uint8_t p2_upper = p2 & 0xFF;
+    uint8_t p2_lower = p2 >> 8 & 0xFF;
+    uint8_t p3_upper = p3 & 0xFF;
+    uint8_t p3_lower = p3 >> 8 & 0xFF;
+
+    snprintf(mac_addr_str_buf, MAC_STR_LEN, "%x:%x:%x:%x:%x:%x", p1_upper, p1_lower, p2_upper, p2_lower, p3_upper, p3_lower);
+    
+    return;
+}
+
+
 
